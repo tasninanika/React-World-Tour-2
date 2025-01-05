a653b09944166f12cf8ffd3dfcf84c2dd148a64d
anikatasnin <jarintasnin09@gmail.com>
2025-01-05 17:53:30 +0600

    updated countries.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css'
+import Countries from './components/Countries/countries'
 
 function App() {
 
   return (
     <>
       <h1>React World Tour</h1>
-      
+      <Countries></Countries>
     </>
   )
 }
