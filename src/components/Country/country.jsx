a723a311065c26797bf769272aed714bf73eb4a3
anikatasnin <jarintasnin09@gmail.com>
2025-01-22 22:26:38 +0600

    updated country.jsx

--- src/components/Country/country.jsx
+++ src/components/Country/country.jsx
@@ -6,6 +6,8 @@ const Country = ({country}) => {
         <div className='country'>
             <h3>Name: {name?.common}</h3>
             <img src={flags?.png} alt="" />
+            <p>Region: {country?.region}</p>
+            <p>Population: {country?.population}</p>
         </div>
     );
 };
