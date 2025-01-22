e572f1b2dacc03b81a898675a2acca59bce05ac4
anikatasnin <jarintasnin09@gmail.com>
2025-01-22 20:29:22 +0600

    updated countries.jsx

--- src/components/Countries/countries.jsx
+++ src/components/Countries/countries.jsx
@@ -1,5 +1,4 @@
-import { useEffect } from "react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const countries = () => {
     const[countries, setCountries] = useState([]);
@@ -8,7 +7,8 @@ const countries = () => {
         fetch('https://restcountries.com/v3.1/all')
         .then(res => res.json())
         .then(data => console.log(data));
-    }, [])
+    })
+    
     
     return (
         <div>
