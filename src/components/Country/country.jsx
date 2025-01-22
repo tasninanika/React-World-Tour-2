22a342bff67e97f57e329c9ccc8cdc352fb8af9c
anikatasnin <jarintasnin09@gmail.com>
2025-01-22 22:46:52 +0600

    updated country.jsx

--- src/components/Country/country.jsx
+++ src/components/Country/country.jsx
@@ -1,7 +1,15 @@
+import { useState } from 'react';
 import './Country.css';
 const Country = ({country}) => {
     console.log(country);
     const {name, flags} = country;
+
+    const [visited, setVisited] = useState(false);
+
+    const handleVisited = () =>{
+        setVisited(true);
+    }
+
     return (
         <div className='country'>
             <h3>Name: {name?.common}</h3>
@@ -9,7 +17,7 @@ const Country = ({country}) => {
             <p>Region: {country?.region}</p>
             <p>Code: {country?.cca2}</p>
             <p>Population: {country?.population}</p>
-            
+            <button onClick={handleVisited}>Visited</button>
         </div>
     );
 };
